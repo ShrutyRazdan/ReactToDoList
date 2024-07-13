2b96248ff70cf215e125431ef2af02129adb032b
Shruty Razdan <shrutyrazdan123@gmail.com>
2024-07-13 11:23:04 +0530

    ReactToDoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { tabs } from './tabs';
 
 function App() {
+  let [todolist,setTodoList]=useState([])
+  
+  let [activeTabs,setactiveTabs]=useState(0)
+  let[activeContent,setactiveContent]=useState(tabs[0])
+  
+  let changeData=(index)=>{
+      setactiveTabs(index)
+      setactiveContent(tabs[index])
+  }
+
+  let saveToDoList=(event)=>{
+
+    let toname=event.target.toname.value;
+     if(!todolist.includes(toname)){
+      let finalDolist=[...todolist,toname]
+      setTodoList(finalDolist)
+}
+    else{
+       alert("ToDo Name Already Exists....");
+}
+    event.preventDefault();
+  }
+
+  let list=todolist.map((value,index)=>{
+     return(
+      <ToDoListItems value={value} key={index} indexNumber={index}
+      todolist={todolist}
+      setTodoList={setTodoList}
+/>
+     )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className='tabsOuter'>
+      <h1 style={{textAlign:'left' }}>
+        Law Prep Vision Mission and Values
+      </h1>
+
+      <ul>
+        {tabs.map((tabsItems,index)=>{
+          return(
+           <li>
+            <button onClick ={()=>changeData(index)}className={activeTabs==index ?'activeButton':''}>{tabsItems.title}</button>
+            </li>
+          )
+        })}
+
+      </ul>
+      
+     { 
+      activeContent!==undefined ?
+      <p>
+          {activeContent.description}
+      </p>
+      :
+      ''
+}
+      </div>
+
+
+      <h1>ToDo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type='text' name='toname'/> <button>Save</button>
+      </form>
+     
+     <div className='outerDiv'>
+      <ul>
+         {list}
+      </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({value,indexNumber,todolist,setTodoList}){
+
+  let [status,setStatus]=useState(false);
+  let deleteRow=()=>{
+     let finalData=todolist.filter((v,i)=>i!=indexNumber)
+     setTodoList(finalData);
+  }
+     let checkStatus=()=>{
+           setStatus(!status);
+     }
+  
+return(
+  <li className={(status)?'completetodo':''} onClick={checkStatus}> {indexNumber+1} {value} <span onClick={deleteRow}>&times;</span></li>
+)
+}
---
+++ src/tabs.jsx
@@ -0,0 +1,17 @@
+ export let tabs=[
+{
+    'title':'Vision',
+    'description': 'Objects are things that we see around us.When we look around, we can find different types of objects around us like pens, pencils, papers, cell phones, chairs, stones, shoes, bottles, and many more. All of them have their unique properties.'
+},
+
+{
+'title':'Mission',
+'description':'Objects are things that we see around us.When we look around, we can find different types of objects around us like pens, pencils, papers, cell phones, chairs, stones, shoes, bottles, and many more. All of them have their unique properties.'
+},
+
+{
+    'title':'Values',
+    'description':'Objects are things that we see around us.When we look around, we can find different types of objects around us like pens, pencils, papers, cell phones, chairs, stones, shoes, bottles, and many more. All of them have their unique properties.'
+},
+
+];
